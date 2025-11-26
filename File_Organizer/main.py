0285b27b190fd6bed53967365190bb1190257766
Šrámek <jakubsramek2006@gmail.com>
2025-11-26 19:09:58 +0100

    Added print statements for feedback

--- File_Organizer/main.py
+++ File_Organizer/main.py
@@ -1,15 +1,5 @@
-import os
-import shutil
+from core.FileMover import move_images
 
-INPUT_FOLDER = r"C:\Users\Uzivatel\Documents"
-OUTPUT_FOLDER = "images"
-
-for f in os.listdir(INPUT_FOLDER):
-    if f.lower().endswith((".jpg", ".png")):
-        src = os.path.join(INPUT_FOLDER, f)
-        dst = os.path.join(OUTPUT_FOLDER, f)
-        os.makedirs(OUTPUT_FOLDER, exist_ok=True)
-        shutil.move(src, dst)
-        print(f"Přesunuto: {f}")
-
-print("Hotovo!")
+if __name__ == "__main__":
+    INPUT_FOLDER = r"C:\Users\Uzivatel\Documents"
+    move_images(INPUT_FOLDER)
