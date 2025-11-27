ff93416aa828e3cbc8fbe3129d471af2e25a106c
Šrámek <jakubsramek2006@gmail.com>
2025-11-27 19:31:21 +0100

    Added Validator import

--- File_Organizer/main.py
+++ File_Organizer/main.py
@@ -1,19 +1,19 @@
-import json
-import os
-from core.Organizer import organize_files
 from lib.ConfigLoader import ConfigLoader
+from ConfigValidator.Validator import Validator
+from core.Organizer import organize_files
 
-CONFIG_FILE = ConfigLoader.get_config_path()
+try:
+    loader = ConfigLoader()
+    raw_config = loader.load_config()
 
-with open(CONFIG_FILE, "r", encoding="utf-8") as f:
-    config = json.load(f)
+    config = Validator.validate(raw_config)
 
-input_folder = config.get("input_folder")
-num_threads = config.get("num_threads", 1)
-output_folders = config.get("output_folders", {})
+    input_folder = config["input_folder"]
+    num_threads = config["num_threads"]
+    output_folders = config["output_folders"]
 
-if not input_folder or not os.path.exists(input_folder):
-    print(f"Chyba: složka '{input_folder}' neexistuje!")
-else:
     print(f"Start přesunu souborů ze složky: {input_folder}")
     organize_files(input_folder, output_folders, num_threads)
+
+except Exception as e:
+    print(f"Chyba při načítání konfigurace nebo spuštění programu: {e}")
