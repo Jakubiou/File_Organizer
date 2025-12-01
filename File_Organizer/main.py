7c3d167eedb32f12913b049ba9143bb370436834
Šrámek <jakubsramek2006@gmail.com>
2025-12-01 20:59:09 +0100

    Added if __name__ == "__main__"

--- File_Organizer/main.py
+++ File_Organizer/main.py
@@ -2,18 +2,21 @@ from lib.ConfigLoader import ConfigLoader
 from File_Organizer.src.ConfigValidator import Validator
 from File_Organizer.src.core.Organizer import organize_files
 
-try:
-    loader = ConfigLoader()
-    raw_config = loader.load_config()
 
-    config = Validator.validate(raw_config)
+if __name__ == "__main__":
+    try:
+        loader = ConfigLoader()
+        raw_config = loader.load_config()
 
-    input_folder = config["input_folder"]
-    num_threads = config["num_threads"]
-    output_folders = config["output_folders"]
+        config = Validator.validate(raw_config)
 
-    print(f"Start přesunu souborů ze složky: {input_folder}")
-    organize_files(input_folder, output_folders, num_threads)
+        input_folder = config["input_folder"]
+        num_threads = config["num_threads"]
+        output_folders = config["output_folders"]
 
-except Exception as e:
-    print(f"Chyba při načítání konfigurace nebo spuštění programu: {e}")
+        print(f"Start přesunu souborů ze složky: {input_folder}")
+        organize_files(input_folder, output_folders, num_threads)
+
+    except Exception as e:
+        print(f"Chyba při načítání konfigurace nebo spuštění programu: {e}")
+    input("\nPress ENTER to exit...")
