83f70bfc53a0d551d8a3982f70268bbaa1c480cb
Šrámek <jakubsramek2006@gmail.com>
2025-12-02 20:57:40 +0100

    method move_file updated added date move system

--- File_Organizer/src/core/FileMover.py
+++ File_Organizer/src/core/FileMover.py
@@ -1,17 +1,40 @@
 import os
 from multiprocessing import Lock
 import shutil
+import datetime
 
 lock = Lock()
 
 
-def move_file(file_path, output_folders):
+def move_file(file_path, output_folders, sort_by_date):
     '''
     Move a file to its corresponding folder based on file extension.
     :param file_path: The full path to the file to move.
     :param output_folders: Dictionary mapping folder names to allowed extensions.
     :return:
     '''
+    if sort_by_date:
+        stat = os.stat(file_path)
+        created = datetime.datetime.fromtimestamp(stat.st_ctime)
+
+        date_folder = created.strftime("%Y-%m-%d")
+
+        os.makedirs(date_folder, exist_ok=True)
+
+        base_name = os.path.basename(file_path)
+        dest_path = os.path.join(date_folder, base_name)
+
+        with lock:
+            count = 1
+            name, extn = os.path.splitext(base_name)
+            while os.path.exists(dest_path):
+                dest_path = os.path.join(date_folder, f"{name}_({count}){extn}")
+                count += 1
+
+            shutil.move(file_path, dest_path)
+            print(f"[DATE MOVE] {file_path} -> {dest_path}")
+        return
+
     ext = file_path.split(".")[-1].lower()
     dest_folder = None
 
