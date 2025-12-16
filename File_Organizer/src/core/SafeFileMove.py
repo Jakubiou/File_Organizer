08f70a7a0ba21ada297a37a0535b42d0bf462d8a
Šrámek <jakubsramek2006@gmail.com>
2025-12-16 22:09:06 +0100

    Added documentation

--- File_Organizer/src/core/SafeFileMove.py
+++ File_Organizer/src/core/SafeFileMove.py
@@ -5,6 +5,12 @@ from multiprocessing import Lock
 move_lock = Lock()
 
 def safe_move(file_path: str, dest_folder: str) -> str:
+    '''
+    Safely moves a file to the destination folder.
+    :param file_path:Path to the file to be moved
+    :param dest_folder: Destination folder where the file will be moved
+    :return: Path to the moved file
+    '''
 
     os.makedirs(dest_folder, exist_ok=True)
 
