3b273ca5a6649464b20cef6de466284f357ae8c8
Šrámek <jakubsramek2006@gmail.com>
2025-12-18 00:38:07 +0100

    Updated method move_file in which was added size moving file if

--- File_Organizer/src/core/FileMover.py
+++ File_Organizer/src/core/FileMover.py
@@ -3,8 +3,7 @@ import datetime
 
 from File_Organizer.src.core.SafeFileMove import safe_move
 
-
-def move_file(file_path, output_folders, use_date_range, date_from, date_to):
+def move_file(file_path, output_folders, mode, date_from, date_to, max_size):
     '''
     Move a file to its corresponding folder based on file extension.
     :param file_path: The full path to the file to move.
@@ -12,25 +11,29 @@ def move_file(file_path, output_folders, use_date_range, date_from, date_to):
     :return:
     '''
 
-    if use_date_range:
+    if not os.path.exists(file_path):
+        return f"[SKIP] Soubor neexistuje: {file_path}"
+
+    if mode == "size":
+        size_mb = os.path.getsize(file_path) / (1024 * 1024)
+        if size_mb <= max_size:
+            final = safe_move(file_path, f"Below_{max_size}_MB")
+            return f"[SIZE MOVE] {file_path} -> {final}"
+        else:
+            return f"[SKIP] {file_path} větší než {max_size} MB"
+
+    if mode == "date":
         stat = os.stat(file_path)
         created = datetime.datetime.fromtimestamp(stat.st_ctime)
-
         if date_from <= created <= date_to:
-            folder_name = f"{date_from.strftime('%Y-%m-%d')}_to_{date_to.strftime('%Y-%m-%d')}"
-            final_path = safe_move(file_path, folder_name)
-            return f"[DATE RANGE MOVE] {file_path} -> {final_path}"
+            folder = f"{date_from.date()}_to_{date_to.date()}"
+            final = safe_move(file_path, folder)
+            return f"[DATE MOVE] {file_path} -> {final}"
 
     ext = file_path.split(".")[-1].lower()
-
-    dest_folder = None
     for folder, extensions in output_folders.items():
         if ext in extensions:
-            dest_folder = folder
-            break
+            final = safe_move(file_path, folder)
+            return f"[TYPE MOVE] {file_path} -> {final}"
 
-    if not dest_folder:
-        return f"[SKIPPED] {file_path} (no folder for .{ext})"
-
-    final_path = safe_move(file_path, dest_folder)
-    return f"[MOVE] {file_path} -> {final_path}"
+    return f"[SKIP] {file_path} – neznámý typ"
