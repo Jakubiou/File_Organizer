50c0b758ccb9e0fbcd98d8733c8a5e4f1279edf2
Šrámek <jakubsramek2006@gmail.com>
2025-12-19 00:52:34 +0100

    Updated main because exe file didnt work correctly before those changes

--- File_Organizer/main.py
+++ File_Organizer/main.py
@@ -1,5 +1,16 @@
-from File_Organizer.src.UI.App import App
+import multiprocessing
+import sys
+import os
+
+if getattr(sys, 'frozen', False):
+    basedir = sys._MEIPASS
+else:
+    basedir = os.path.dirname(os.path.abspath(__file__))
+
+from src.UI.App import App
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
+
     app = App()
     app.run()
